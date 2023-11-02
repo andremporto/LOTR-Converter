b2cf21ddb9612b5147a895082cba1670644fcb16
André Porto <andreporto@me.com>
2023-11-02 09:27:53 -0300

    gitflow-release-stash: 1.0
    App is first version ready

--- LOTR Converter/Currency.swift
+++ LOTR Converter/Currency.swift
@@ -13,14 +13,28 @@ enum Currency: Double, CaseIterable {
     case silverPiece = 16
     case goldPenny = 4
     case goldPiece = 1
+    
+    func convert(amountString: String, to currency: Currency) -> String {
+        guard let startAmount = Double(amountString) else {
+            return ""
+        }
+        
+        let convertedAmount = (startAmount / self.rawValue) * currency.rawValue
+        
+        if convertedAmount > 0 {
+            return String(format: "%.2f", convertedAmount)
+        } else {
+            return ""
+        }
+    }
 }
 
 enum CurrencyText: String, CaseIterable {
     case copperPenny = "Moeda de Cobre"
     case silverPenny = "Moeda de Prata"
-    case silverPiece = "Peça de Prata"
+    case silverPiece = "Prata"
     case goldPenny = "Moeda de Ouro"
-    case goldPiece = "Peça de Ouro"
+    case goldPiece = "Ouro"
 }
 
 enum CurrencyImage: String, CaseIterable {
